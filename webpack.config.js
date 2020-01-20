0fded09406f8890e533d52dd932fbd34459886e7
Stephen Axtell <axtell.stephen@gmail.com>
2020-01-20 15:54:49 +0200

    implemented cache busting with contentHashing and added the WebpackHTML plugin, which is needed when doing content hashing

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development', // by default mode is set to production and minifies the js, here it doesnt
 	entry: './src/index.js',
 	output: {
-		filename: 'main.js',
+		filename: 'main.[contentHash].js',
 		path: path.resolve(__dirname, 'dist')
 	},
+	plugins: [new HtmlWebpackPlugin({
+		template: './src/template.html'
+	})],
 	module: {
 		rules: [
 			{
