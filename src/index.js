d5daffccba6fbc88c7d928622c19fdfb20907f6f
Stephen Axtell <axtell.stephen@gmail.com>
2020-01-20 13:30:03 +0200

    initial conversion from old school site to one using modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('Hello from Webpack');
+import { run } from './app/app';
+import {AlertService} from './app/alert.service';
+import { ComponentService } from './app/component.service';
+
+const aService = new AlertService();
+const cService = new ComponentService();
+
+run(aService, cService);
